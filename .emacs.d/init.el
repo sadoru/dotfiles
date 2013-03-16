a46c29a91e81a949671789eaf526da3111cea79b
sadoru <sadoroo@gmail.com>
2013-03-17 01:46:04 +0900

    package取得先に http://melpa.milkbox.net/ も追加しておく（カットしてたのを有効化）

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,7 +23,7 @@
 ;; packages
 ;;------------------------------------------------------------------
 (require 'package)
-;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
